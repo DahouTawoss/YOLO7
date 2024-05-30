0880460e1e163c59b7b356676a58afa4a9d7f4d8
DahouTawoss <menmecho@gmail.com>
2024-05-30 21:30:28 +0100

    Update kalman_filter.py

--- strong_sort/sort/kalman_filter.py
+++ strong_sort/sort/kalman_filter.py
@@ -38,14 +38,20 @@ class KalmanFilter(object):
         for i in range(ndim):
             self._motion_mat[i, ndim + i] = dt
 
+        # c'est la matrice H des observations
         self._update_mat = np.eye(ndim, 2 * ndim)
 
         # Motion and observation uncertainty are chosen relative to the current
         # state estimate. These weights control the amount of uncertainty in
         # the model. This is a bit hacky.
+
+        # Ils contrôlent la quantité d'incertitude dans la position et la vitesse respectivement.
         self._std_weight_position = 1. / 20
         self._std_weight_velocity = 1. / 160
 
+
+    # créer une nouvelle piste à partir d'une détection non associée
+    # retourner la moyenne et la covariance des nouvelles pistes
     def initiate(self, measurement):
         """Create track from unassociated measurement.
         Parameters
@@ -60,8 +66,10 @@ class KalmanFilter(object):
             dimensional) of the new track. Unobserved velocities are initialized
             to 0 mean.
         """
-        mean_pos = measurement
-        mean_vel = np.zeros_like(mean_pos)
+
+        mean_pos = measurement # les positions de la bounding box
+        mean_vel = np.zeros_like(mean_pos) # la vitesse ( velocity)
+        # le mean dans ce cas est l'etat du systeme xk
         mean = np.r_[mean_pos, mean_vel]
 
         std = [
@@ -76,6 +84,9 @@ class KalmanFilter(object):
         covariance = np.diag(np.square(std))
         return mean, covariance
 
+
+    #utilise la matrice de transition _motion_mat pour prédire l'état futur à partir de l'état actuel
+    #Elle met à jour la covariance en tenant compte de l'incertitude du modèle de mouvement
     def predict(self, mean, covariance):
         """Run Kalman filter prediction step.
         Parameters
@@ -106,7 +117,7 @@ class KalmanFilter(object):
 
         mean = np.dot(self._motion_mat, mean)
         covariance = np.linalg.multi_dot((
-            self._motion_mat, covariance, self._motion_mat.T)) + motion_cov
+            self._motion_mat, covariance, self._motion_mat.T)) + motion_cov #la predection de la cobariance P_k^- = F_k P_{k-1} F_k^T + Q_k
 
         return mean, covariance
 
@@ -137,6 +148,8 @@ class KalmanFilter(object):
         innovation_cov = np.diag(np.square(std))
 
         mean = np.dot(self._update_mat, mean)
+        #pour le calcule de gain de kalman
+        # cette covariance est pour calculer seulement (H_k P_k^- H_k^T + R_k)^{-1} de K_k = P_k^- H_k^T (H_k P_k^- H_k^T + R_k)^{-1}
         covariance = np.linalg.multi_dot((
             self._update_mat, covariance, self._update_mat.T))
         return mean, covariance + innovation_cov
@@ -163,14 +176,17 @@ class KalmanFilter(object):
 
         chol_factor, lower = scipy.linalg.cho_factor(
             projected_cov, lower=True, check_finite=False)
-        kalman_gain = scipy.linalg.cho_solve(
-            (chol_factor, lower), np.dot(covariance, self._update_mat.T).T,
-            check_finite=False).T
+
+        kalman_gain = scipy.linalg.cho_solve((chol_factor, lower), np.dot(covariance, self._update_mat.T).T,check_finite=False).T
+
+        # measurement dans ce cas est le zk
         innovation = measurement - projected_mean
 
+        # l mean dans ce cas est le xk
+        # state update
         new_mean = mean + np.dot(innovation, kalman_gain.T)
-        new_covariance = covariance - np.linalg.multi_dot((
-            kalman_gain, projected_cov, kalman_gain.T))
+        #mise a jour de la covariance de l'erreur d'estimation P_k^+ = (I - K_k H_k) P_k^-
+        new_covariance = covariance - np.linalg.multi_dot((kalman_gain, projected_cov, kalman_gain.T))
         return new_mean, new_covariance
 
     def gating_distance(self, mean, covariance, measurements,
@@ -205,6 +221,8 @@ class KalmanFilter(object):
             mean, covariance = mean[:2], covariance[:2, :2]
             measurements = measurements[:, :2]
 
+
+        # mahalanobis distance
         cholesky_factor = np.linalg.cholesky(covariance)
         d = measurements - mean
         z = scipy.linalg.solve_triangular(
