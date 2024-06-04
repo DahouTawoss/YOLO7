d7ed5aab32b5c579b2ec74396c71024a38cce246
DahouTawoss <menmecho@gmail.com>
2024-06-04 15:53:57 +0100

    Update track.py

--- track.py
+++ track.py
@@ -231,9 +231,14 @@ def run(
                             bbox_w = output[2] - output[0]
                             bbox_h = output[3] - output[1]
                             # Write MOT compliant results to file
+                            save_format = '{frame},{id},{x1},{y1},{w},{h},{x},{y},{z}\n'
                             with open(txt_path + '.txt', 'a') as f:
-                                f.write(('%g ' * 10 + '\n') % (frame_idx + 1, id, bbox_left,  # MOT format
-                                                               bbox_top, bbox_w, bbox_h, -1, -1, -1, i))
+                              line = save_format.format(frame=frame_idx, id=id, x1=bbox_left, y1=bbox_top, w=bbox_w, h=bbox_h, x = -1, y = -1, z = -1)
+                              f.write(line)
+
+                            """with open(txt_path + '.txt', 'a') as f:
+                                f.write(('%g ' * 9 + '\n') % (frame_idx + 1, id, bbox_left,  # MOT format
+                                                               bbox_top, bbox_w, bbox_h, -1, -1, -1))"""
 
                         if save_vid or save_crop or show_vid:  # Add bbox to image
                             c = int(cls)  # integer class
